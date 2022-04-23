0a878a074ee1aa6048b15aad6340d57bc3d7d557
Ilya Pakhalko <ilyaphlk@github.com>
2022-04-24 01:17:54 +0300

    add configs for sst2

---
+++ seq2seq/scripts/sst2/sst2_expansion.sh
@@ -0,0 +1,3 @@
+python3 run_seq2seq.py  configs/sst2/expansion/expansion_1.json
+python3 run_seq2seq.py  configs/sst2/expansion/reduction_8.json
+python3 run_seq2seq.py  configs/sst2/expansion/reduction_32.json
---
+++ seq2seq/scripts/sst2/sst2_tt_d.sh
@@ -0,0 +1,3 @@
+python3 run_seq2seq.py  configs/sst2/tt_d/tt_d_2.json
+python3 run_seq2seq.py  configs/sst2/tt_d/tt_d_4.json
+python3 run_seq2seq.py  configs/sst2/tt_d/tt_d_5.json
