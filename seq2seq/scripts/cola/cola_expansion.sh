6419947fcb2f952b384b93b0a485fb0377591279
Ilya Pakhalko <ilyaphlk@github.com>
2022-04-23 19:41:51 +0300

    organize scripts

---
+++ seq2seq/scripts/cola/cola_expansion.sh
@@ -0,0 +1,5 @@
+python run_seq2seq.py  configs/cola/expansion/expansion_32.json
+python run_seq2seq.py  configs/cola/expansion/expansion_8.json
+python run_seq2seq.py  configs/cola/expansion/expansion_1.json
+python run_seq2seq.py  configs/cola/expansion/reduction_8.json
+python run_seq2seq.py  configs/cola/expansion/reduction_32.json
