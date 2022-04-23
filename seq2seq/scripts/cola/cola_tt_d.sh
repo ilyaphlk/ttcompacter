64c09ca1a872bc5632789c156e9e28c49cc73e8f
Ilya Pakhalko <ilyaphlk@github.com>
2022-04-23 21:13:48 +0300

    fix scripts

--- seq2seq/scripts/cola/cola_tt_d.sh
+++ seq2seq/scripts/cola/cola_tt_d.sh
@@ -1,3 +1,3 @@
-python run_seq2seq.py  configs/cola/tt_d/tt_d_2.json
-python run_seq2seq.py  configs/cola/tt_d/tt_d_4.json
-python run_seq2seq.py  configs/cola/tt_d/tt_d_5.json
+python3 run_seq2seq.py  configs/cola/tt_d/tt_d_2.json
+python3 run_seq2seq.py  configs/cola/tt_d/tt_d_4.json
+python3 run_seq2seq.py  configs/cola/tt_d/tt_d_5.json
--- seq2seq/scripts/cola/cola_tt_rank.sh
+++ seq2seq/scripts/cola/cola_tt_rank.sh
@@ -1,3 +1,3 @@
-python run_seq2seq.py  configs/cola/tt_rank/tt_rank_2.json
-python run_seq2seq.py  configs/cola/tt_rank/tt_rank_4.json
-python run_seq2seq.py  configs/cola/tt_rank/tt_rank_16.json
+python3 run_seq2seq.py  configs/cola/tt_rank/tt_rank_2.json
+python3 run_seq2seq.py  configs/cola/tt_rank/tt_rank_4.json
+python3 run_seq2seq.py  configs/cola/tt_rank/tt_rank_16.json
