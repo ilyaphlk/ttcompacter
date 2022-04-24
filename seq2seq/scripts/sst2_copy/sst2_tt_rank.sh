0da20017980448cb0f42eb118b505b765dbe967d
Ilya Pakhalko <ilyaphlk@github.com>
2022-04-24 15:01:35 +0300

    add configs

---
+++ seq2seq/scripts/sst2_copy/sst2_tt_rank.sh
@@ -0,0 +1,3 @@
+python3 run_seq2seq.py  configs/sst2/tt_rank/tt_rank_2.json
+python3 run_seq2seq.py  configs/sst2/tt_rank/tt_rank_4.json
+python3 run_seq2seq.py  configs/sst2/tt_rank/tt_rank_16.json
