7dc40057fac09a5cf284cd0cbbb9785b0b0b689a
Ilya Pakhalko <ilyaphlk@github.com>
2022-05-07 19:57:39 +0300

    add logging of grad_norm

--- seq2seq/third_party/trainers/trainer.py
+++ seq2seq/third_party/trainers/trainer.py
@@ -764,8 +764,9 @@ class BaseTrainer(Trainer):
                     # calculate global grad norm
 
                     for p in model.parameters():
-                        param_norm = p.grad.detach().data.norm(2)
-                        total_norm += param_norm.item() ** 2
+                        if p.grad is not None:
+                            param_norm = p.grad.detach().data.norm(2)
+                            total_norm += param_norm.item() ** 2
                     total_norm = total_norm ** 0.5
                     #########################
 
