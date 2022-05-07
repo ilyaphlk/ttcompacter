efac7eb786fb56d32af81637996586c07732dbe6
Ilya Pakhalko <ilyaphlk@github.com>
2022-05-08 02:11:42 +0300

    try to optimize

--- seq2seq/third_party/trainers/trainer.py
+++ seq2seq/third_party/trainers/trainer.py
@@ -767,7 +767,7 @@ class BaseTrainer(Trainer):
 
                     if (self.global_step + 1) % args.logging_steps == 0:
                         for p in model.parameters():
-                            if p.grad is not None
+                            if p.grad is not None:
                                 param_norm = p.grad.detach().data.norm(2)
                                 total_norm += param_norm.item() ** 2
                         total_norm = total_norm ** 0.5
