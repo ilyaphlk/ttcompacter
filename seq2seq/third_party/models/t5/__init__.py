3b3269b342c5b8fd5d5f68882da718572faf514b
Ilya Pakhalko <ilyaphlk@github.com>
2022-05-13 15:19:44 +0300

    add lora support

--- seq2seq/third_party/models/t5/__init__.py
+++ seq2seq/third_party/models/t5/__init__.py
@@ -1,2 +1,2 @@
-from .modeling_t5 import T5ForConditionalGeneration , T5LayerNorm
+from .modeling_t5 import T5ForConditionalGeneration , T5LayerNorm, TTLoRALinear, LoRALinear
 from .configuration_t5 import T5Config
