ed43d948ca361296ecce440a24506dbd8a718cf9
Ilya Pakhalko <ipahalko@gmail.com>
2022-10-05 04:33:47 +0300

    update datasets version

--- setup.py
+++ setup.py
@@ -39,14 +39,14 @@ def setup_package():
       ],
       cmdclass={"build_ext": BuildExtension},
       install_requires=[
-        'datasets==2.5.1',
-        'scikit-learn==0.24.2',
-        'tensorboard==2.5.0',
-        'matplotlib==3.4.2',
+        'datasets',
+        'scikit-learn',
+        'tensorboard',
+        'matplotlib',
         #'ttpy==1.2.1',
         #'torch==1.8.0+cu111',
         #'torch==1.8.0',
-        'transformers==4.22.2'
+        'transformers'
       ],
   )
 
